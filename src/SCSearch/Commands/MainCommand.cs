d6f7953d96d8b274a21c24e16e08c1db392724c2
Funny_Silkie <funnysilkie@gmail.com>
2024-09-19 01:57:03 +0900

    init vs

---
+++ src/SCSearch/Commands/MainCommand.cs
@@ -0,0 +1,17 @@
+﻿using CuiLib.Commands;
+
+namespace SCSearch.Commands
+{
+    /// <summary>
+    /// メインのコマンドを表します。
+    /// </summary>
+    internal class MainCommand : Command
+    {
+        /// <summary>
+        /// <see cref="MainCommand"/>の新しインスタンスを初期化します。
+        /// </summary>
+        public MainCommand() : base("scsearch")
+        {
+        }
+    }
+}
---
+++ src/SCSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using CuiLib;
+using SCSearch.Commands;
+
+namespace SCSearch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var command = new MainCommand();
+            if (args.Length == 0)
+            {
+                command.WriteHelp(Console.Out);
+                return;
+            }
+
+            try
+            {
+                command.Invoke(args);
+            }
+            catch (ArgumentAnalysisException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
+}
